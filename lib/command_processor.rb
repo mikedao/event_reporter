c833a4398e8ead06849f1229ed46afa55525896a
Michael Dao <michael.dao@gmail.com>
2014-11-15 20:57:30 -0700

    Added help files.

--- lib/command_processor.rb
+++ lib/command_processor.rb
@@ -1,4 +1,9 @@
+require_relative 'display'
+
 class CommandProcessor
+  attr_reader   :command,
+                :instream,
+                :outstream
 
   def initialize(instream, outstream)
     @instream   = instream
@@ -7,9 +12,32 @@ class CommandProcessor
   end
 
   def process_command(command)
+    @command = command
+    case
+    when help_only?
+      outstream.puts Display.help_only
+    when help_queue_only?
+      outstream.puts Display.help_queue_only
+    end
+
+
+  end
+
+  def help_only?
+    command[0] == "help" && command[1].nil?
+  end
+
+  def not_help_only?
+    command[0] == "help" && !command[1].nil?
   end
 
   def finished?(input)
     input == ["q"] || input == ["quit"]
   end
+
+  def help_queue_only?
+    command[0] == "help" && command[1] && command[2].nil?
+  end
+
+
 end
