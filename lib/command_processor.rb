b07b3996aa5bcb57f9f6b072dd2e0c9905094caa
Michael Dao <michael.dao@gmail.com>
2014-11-16 11:51:01 -0700

    Got file loading working

--- lib/command_processor.rb
+++ lib/command_processor.rb
@@ -5,12 +5,14 @@ require_relative 'csvhandler'
 class CommandProcessor
   attr_reader   :command,
                 :instream,
-                :outstream
+                :outstream,
+                :csvfile
 
   def initialize(instream, outstream)
     @instream   = instream
     @outstream  = outstream
     @command    = ""
+    @csvfile    = CSVHandler.new
   end
 
   def process_command(entered_command)
@@ -18,10 +20,12 @@ class CommandProcessor
     case
     when command == ""
         outstream.puts Display.invalid_command
+    when command == "q"
+        outstream.puts Display.quit_message
     when command[0..3] == "help"
       Help.show_help(instream,outstream,command)
     when command[0..3] == "load"
-      puts "call load method"
+      load_file(command)
     when command[0..4] == "queue"
       puts "call queue method"
     when command[0..3] == "find"
@@ -30,6 +34,19 @@ class CommandProcessor
       outstream.puts Display.invalid_command
     end
   end
+
+  def load_file(entered_command)
+    if entered_command.split[1] == nil
+      filename = "event_attendees.csv"
+    else
+      filename = entered_command.split[1]
+    end
+    csvfile.load_file(filename)
+    outstream.puts csvfile.data[0]
+  end
+
+
+
 
   def finished?(entered_command)
     entered_command = "quit" || entered_command = "q"
