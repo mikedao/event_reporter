2e07d04bbeb25d3b72f0172d74efbd66d2fd83db
Michael Dao <michael.dao@gmail.com>
2014-11-19 15:07:00 -0700

    Added Tab delimited

--- lib/csvhandler.rb
+++ lib/csvhandler.rb
@@ -36,4 +36,42 @@ class CSVHandler
     "RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode"
   end
 
+  def txt_header
+    "RegDate\tfirst_Name\tlast_Name\tEmail_Address\tHomePhone\tStreet\tCity\tState\tZipcode"
+  end
+
+  def txt_save_to(data)
+    "#{data.send("regdate")}\t#{data.send("first_name")}\t#{data.send("last_name")}\t#{data.send("email_address")}\t#{data.send("homephone")}\t#{data.send("street")}\t#{data.send("city")}\t#{data.send("state")}\t#{data.send("zipcode")}"
+  end
+
+
+  def save_json(filename, data)
+    File.open(filename, "w") do |file|
+      file.puts "["
+    end
+
+    File.open(filename, 'a') do |file|
+
+      data.map do |data|
+
+        file.puts json_data(data)
+      end
+      file.puts "]"
+    end
+  end
+
+  def json_data(data)
+    "{
+    \"RegDate\":\"#{data.send(regdate)}\",
+    \"first_Name\":\"#{data.send(first_name)}\",
+    \"last_Name\":\"#{data.send(last_name)}\",
+    \"Email_Address\":\"#{data.send(email_address)}\",
+    \"HomePhone\":\"#{data.send(homephone)}\",
+    \"Street\":\"#{data.send(street)}\",
+    \"City\":\"#{data.send(city)}\",
+    \"State\":\"#{data.send(state)}\",
+    \"Zipcode\":#{data.send(zipcode)},
+    "
+  end
+
 end
